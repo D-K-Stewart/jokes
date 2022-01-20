5589115f36655e2187080e3b651fff4ec83d0626
D-K-Stewart <90126394+D-K-Stewart@users.noreply.github.com>
2022-01-19 16:48:48 -0800

    Initial commit

---
+++ server/models/jokes.models.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const JokeSchema = new mongoose.Schema({
+	setup: {
+		type: String,
+		required: [true, "First name is required"],
+		minlength: [10,
+		"Joke must be at least 10 characters long"]
+	},
+	punchline: {
+		type: String,
+		required: [true,
+		"Last name is required"],
+		minlength: [3,
+		"Punchline must be at least 3 characters long" ]
+	},
+	},
+	{timestamps: true}
+
+);
+	
+const Joke = mongoose.model("Joke", JokeSchema)
+
+module.exports = Joke;
